7201f4f815322b076b36e0c0b2f1ddbb3276897e
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-05-04 21:32:17 -0400

    image moves side to side

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"image"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var _ embed.FS // keeps embed imported even if not directly used
+//go:embed assets/images/key.png
+var keyPNG []byte
+
+
+
+const (
+    screenWidth     = 256
+    screenHeight    = 256
+    scale           = 3   
+    frameWidth      = 16
+    frameHeight     = 16
+)
+
+var keyImage *ebiten.Image
+var count int = 0
+var direction bool
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	if count % screenWidth * scale == 0 {
+		direction = !direction
+	}
+	if direction{
+		count++}
+	if !direction{
+		count--}
+	// if count > screenWidth * scale {
+	// }
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Chess Rescue!")
+	
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
+	// op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	op.GeoM.Translate(float64(count), 50)
+	screen.DrawImage(keyImage, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	// Decode an image from the image file's byte slice.
+
+	img, _, err := image.Decode(bytes.NewReader(keyPNG))
+	if err != nil {
+			log.Fatal(err)
+	}
+	keyImage = ebiten.NewImageFromImage(img)
+
+
+	ebiten.SetWindowSize(screenWidth*scale, screenHeight*scale)
+	ebiten.SetWindowTitle("Chess Rescue")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Chess Rescue
+
+Learning Go and Ebitengine (really learning it!) via making a platformer style game.
+You will be rescuing captured chess pieces.
+Pawn levels (easiest) all the way to the king and queen (hardest)
+
+## Iterations.
+
+1. Add in an image and have it move side to side!
+2. ...
