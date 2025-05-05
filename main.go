c3b1f3bb87ce08f4e2688261271f9c93ea882d72
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-05-04 21:46:59 -0400

    test out up and down arrows to move the key up and down

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 var _ embed.FS // keeps embed imported even if not directly used
@@ -28,8 +29,11 @@ const (
 var keyImage *ebiten.Image
 var count int = 0
 var direction bool
+var playerY float64 = 50.0
 
-type Game struct{}
+type Game struct{
+	keys []ebiten.Key
+}
 
 func (g *Game) Update() error {
 	if count % screenWidth * scale == 0 {
@@ -41,6 +45,18 @@ func (g *Game) Update() error {
 		count--}
 	// if count > screenWidth * scale {
 	// }
+	
+	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		playerY--
+		log.Println("Arrow Up pressed - do something")
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		playerY++
+		log.Println("Arrow Down pressed - do something else")
+	}
 
 	return nil
 }
@@ -51,7 +67,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
 	// op.GeoM.Translate(screenWidth/2, screenHeight/2)
-	op.GeoM.Translate(float64(count), 50)
+	op.GeoM.Translate(float64(count), playerY)
 	screen.DrawImage(keyImage, op)
 }
 
