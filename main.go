c5d3a91ba9d9adfd85429bf2c781aa7cb9ef5ada
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-05-05 22:27:44 -0400

    A bit of Y axis edge detection and more debug statements

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"embed"
+	"fmt"
 	"image"
 	_ "image/png"
 	"log"
@@ -12,7 +13,9 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-var _ embed.FS // keeps embed imported even if not directly used
+var _ embed.FS // keeps embed imported to fool the Go Linter
+// this is where the image gets loaded in, the embed is *supposed* to be commented. It's special and not really a comment.
+
 //go:embed assets/images/key.png
 var keyPNG []byte
 
@@ -25,17 +28,28 @@ const (
 )
 
 var (
-	keyImage  *ebiten.Image
-	// count     int = 0
-	// direction bool
-	// playerY   float64 = 50.0
+	keyImage *ebiten.Image
 )
 
 type Game struct {
-	keys []ebiten.Key
+	keys 			[]ebiten.Key
 	count     int
 	direction bool
 	playerY   float64
+}
+
+func edgeCheck(x, y int, g *Game) bool{
+	if x == 0 || x == screenWidth*scale {
+		fmt.Println("On x edge")
+		return true
+	}
+	if y == 1 || y == screenHeight - 1 {
+		fmt.Println("On y edge")
+		if y < frameHeight {g.playerY += frameHeight}
+		if y > screenHeight-frameHeight{g.playerY -= frameHeight}
+		return true
+	}
+	return false
 }
 
 func (g *Game) Update() error {
@@ -53,11 +67,11 @@ func (g *Game) Update() error {
 
 	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
 
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && !edgeCheck(g.count, int(g.playerY), g){
 		g.playerY--
 	}
 
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && !edgeCheck(g.count, int(g.playerY), g){
 		g.playerY++
 	}
 
@@ -65,7 +79,10 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Chess Rescue!")
+	msg := fmt.Sprintf(`Chess Rescue!
+	player y: %v
+	count/x: %v`, g.playerY, g.count)
+	ebitenutil.DebugPrint(screen, msg)
 
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
@@ -80,7 +97,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	// Decode an image from the image file's byte slice.
-
 	img, _, err := image.Decode(bytes.NewReader(keyPNG))
 	if err != nil {
 		log.Fatal(err)
@@ -93,7 +109,7 @@ func main() {
 	game := &Game{
 		count:     0,
 		direction: false,
-		playerY:   50.0,
+		playerY:   screenHeight/2,
 	}
 
 	if err := ebiten.RunGame(game); err != nil {
